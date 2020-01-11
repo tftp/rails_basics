d2bb4e61efe794d63f417421794990f171721944
Oleg Goloborodko <goloborodko@inbox.ru>
2020-01-11 13:56:52 +0300

    add class time_formatter

--- rack/app.rb
+++ rack/app.rb
@@ -1,46 +1,10 @@
 class App
+  require_relative 'time_formatter'
 
   def call(env)
 
-    params = processing_parameters(env)
-    [params[:status], headers, params[:body]]
+    params = TimeFormatter.new(env).start
+    [params[:status], params[:headers], params[:body]]
   end
-
-end
-
-private
-
-def headers
-  { 'Content-Type' => 'text/plain' }
-end
-
-def processing_parameters(env)
-  params = {}
-  query_string = env['QUERY_STRING'].split('=')
-
-  if query_string[0] != 'format' || env['REQUEST_PATH'] != '/time' || query_string[1].nil?
-    params[:body] = ["Not found \n"]
-    params[:status] = 404
-    return params
-  end
-
-  available_format = %w(year month day hour minute second)
-  received_format = query_string[1].nil? ? '' : env['QUERY_STRING'].split('=')[1].split('-')
-  unknown_format = received_format - available_format
-
-  unless unknown_format.empty?
-    params[:body] = ["Unknown time format [#{unknown_format.join(', ')}] \n"]
-    params[:status] = 400
-    return params
-  end
-
-  received_format.map! do |format|
-    format = 'min' if format == 'minute'
-    format = 'sec' if format == 'second'
-    Time.now.send(format.to_sym)
-  end
-  params[:body] = [received_format.join('-')]
-  params[:status] = 200
-  return params
 
 end
